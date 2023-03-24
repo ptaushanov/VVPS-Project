a65e7100583a05ea603beea55ee26338653322d2
ptaushanov <pepi15032000@gmail.com>
2023-03-24 10:27:13 +0200

    refactor: refactored some unit tests

--- VVPS.UnitTests/TicketPricingTests.cs
+++ VVPS.UnitTests/TicketPricingTests.cs
@@ -10,8 +10,6 @@ namespace VVPS.UnitTests
         private string FromCity { get; set; }
         private string ToCity { get; set; }
 
-        private Dictionary<string, double> PricingTable { get; set; }
-
         private DiscountCard DiscountCard { get; set; }
 
         #region Setup
@@ -22,8 +20,6 @@ namespace VVPS.UnitTests
             FromCity = "Sofia";
             ToCity = "Varna";
 
-            PricingTable = new Dictionary<string, double>() { { "Sofia-Varna", 50.0 } };
-
             DiscountCard = new FamilyDiscountCard();
         }
 
@@ -32,55 +28,17 @@ namespace VVPS.UnitTests
         #region CalculatePriceFromTimeOfDayTests
 
         [Test]
-        public void CalculatePriceFromTimeOfDay_ShouldThrowNullReferenceException_WhenPricingTableIsNull()
-        {
-            // Arrange
-            TicketPricing.PricingTable = null;
-            MethodInfo? methodInfo = typeof(TicketPricing).GetMethod(
-                "CalculatePriceFromTimeOfDay",
-                BindingFlags.NonPublic | BindingFlags.Static
-            );
-            object[] parameters = { FromCity, ToCity, DateTime.Now };
-
-            // Act & Assert
-            var exception = Assert.Throws<TargetInvocationException>(
-                () => methodInfo?.Invoke(null, parameters)
-            );
-            Assert.That(exception.InnerException, Is.InstanceOf<NullReferenceException>());
-        }
-
-        [Test]
-        public void CalculatePriceFromTimeOfDay_ShouldThrowArgumentException_WhenSearchKeyNotPresent()
-        {
-            // Arrange
-            TicketPricing.PricingTable = PricingTable;
-            MethodInfo? methodInfo = typeof(TicketPricing).GetMethod(
-                "CalculatePriceFromTimeOfDay",
-                BindingFlags.NonPublic | BindingFlags.Static
-            );
-            string fromCity = "random";
-            string toCity = "random";
-            object[] parameters = { fromCity, toCity, DateTime.Now };
-
-            // Act & Assert
-            var exception = Assert.Throws<TargetInvocationException>(
-                () => methodInfo?.Invoke(null, parameters)
-            );
-            Assert.That(exception.InnerException, Is.InstanceOf<ArgumentException>());
-        }
-
-        [Test]
         public void CalculatePriceFromTimeOfDay_ShouldReturnBasePrice_WhenLessThenMorningTime()
         {
             // Arrange
-            TicketPricing.PricingTable = PricingTable;
             MethodInfo? methodInfo = typeof(TicketPricing).GetMethod(
                 "CalculatePriceFromTimeOfDay",
                 BindingFlags.NonPublic | BindingFlags.Static
             );
-            object[] parameters = { FromCity, ToCity, new DateTime(2023, 03, 17, 8, 0, 0) };
+
             double basePrice = 50.0;
             double tolerance = 0.0001;
+            object[] parameters = { basePrice, new DateTime(2023, 03, 17, 8, 0, 0) };
 
             // Act
             object? result = methodInfo?.Invoke(null, parameters);
@@ -94,14 +52,15 @@ namespace VVPS.UnitTests
         public void CalculatePriceFromTimeOfDay_ShouldReturnDiscountedPrice_WhenBetweenMorningAndAfternoonTime()
         {
             // Arrange
-            TicketPricing.PricingTable = PricingTable;
             MethodInfo? methodInfo = typeof(TicketPricing).GetMethod(
                 "CalculatePriceFromTimeOfDay",
                 BindingFlags.NonPublic | BindingFlags.Static
             );
-            object[] parameters = { FromCity, ToCity, new DateTime(2023, 03, 17, 10, 0, 0) };
+            double basePrice = 50.0;
             double discountedPrice = 47.5;
             double tolerance = 0.0001;
+
+            object[] parameters = { basePrice, new DateTime(2023, 03, 17, 10, 0, 0) };
 
             // Act
             object? result = methodInfo?.Invoke(null, parameters);
@@ -115,14 +74,14 @@ namespace VVPS.UnitTests
         public void CalculatePriceFromTimeOfDay_ShouldReturnBasePrice_WhenBetweenAfternoonAndEveningTime()
         {
             // Arrange
-            TicketPricing.PricingTable = PricingTable;
             MethodInfo? methodInfo = typeof(TicketPricing).GetMethod(
                 "CalculatePriceFromTimeOfDay",
                 BindingFlags.NonPublic | BindingFlags.Static
             );
-            object[] parameters = { FromCity, ToCity, new DateTime(2023, 03, 17, 17, 30, 0) };
+
             double basePrice = 50.0;
             double tolerance = 0.0001;
+            object[] parameters = { basePrice, new DateTime(2023, 03, 17, 17, 30, 0) };
 
             // Act
             object? result = methodInfo?.Invoke(null, parameters);
@@ -132,19 +91,19 @@ namespace VVPS.UnitTests
             Assert.That(resultPrice, Is.EqualTo(basePrice).Within(tolerance));
         }
 
-        // test for CalculatePriceFromTimeOfDay_ShouldReturnDiscountedPrice_WhenGreaterThenEveningTime
         [Test]
         public void CalculatePriceFromTimeOfDay_ShouldReturnDiscountedPrice_WhenGreaterThenEveningTime()
         {
             // Arrange
-            TicketPricing.PricingTable = PricingTable;
             MethodInfo? methodInfo = typeof(TicketPricing).GetMethod(
                 "CalculatePriceFromTimeOfDay",
                 BindingFlags.NonPublic | BindingFlags.Static
             );
-            object[] parameters = { FromCity, ToCity, new DateTime(2023, 03, 17, 20, 0, 0) };
+            double basePrice = 50.0;
             double discountedPrice = 47.5;
             double tolerance = 0.0001;
+
+            object[] parameters = { basePrice, new DateTime(2023, 03, 17, 20, 0, 0) };
 
             // Act
             object? result = methodInfo?.Invoke(null, parameters);
@@ -159,59 +118,19 @@ namespace VVPS.UnitTests
         #region CalculatePriceFromDiscountTests
 
         [Test]
-        public void CalculatePriceFromDiscount_ShouldThrowNullReferenceException_WhenPricingTableIsNull()
-        {
-            // Arrange
-            TicketPricing.PricingTable = null;
-            MethodInfo? methodInfo = typeof(TicketPricing).GetMethod(
-                "CalculatePriceFromDiscount",
-                BindingFlags.NonPublic | BindingFlags.Static
-            );
-            bool childUnder16Present = false;
-            object[] parameters = { FromCity, ToCity, DiscountCard, childUnder16Present };
-
-            // Act & Assert
-            var exception = Assert.Throws<TargetInvocationException>(
-                () => methodInfo?.Invoke(null, parameters)
-            );
-            Assert.That(exception.InnerException, Is.InstanceOf<NullReferenceException>());
-        }
-
-        [Test]
-        public void CalculatePriceFromDiscount_ShouldThrowArgumentException_WhenSearchKeyNotPresent()
-        {
-            // Arrange
-            TicketPricing.PricingTable = PricingTable;
-            MethodInfo? methodInfo = typeof(TicketPricing).GetMethod(
-                "CalculatePriceFromDiscount",
-                BindingFlags.NonPublic | BindingFlags.Static
-            );
-            string fromCity = "randomText";
-            string toCity = "randomText";
-            bool childUnder16Present = false;
-            object[] parameters = { fromCity, toCity, DiscountCard, childUnder16Present };
-
-            // Act & Assert
-            var exception = Assert.Throws<TargetInvocationException>(
-                () => methodInfo?.Invoke(null, parameters)
-            );
-            Assert.That(exception.InnerException, Is.InstanceOf<ArgumentException>());
-        }
-
-        [Test]
         public void CalculatePriceFromDiscount_ShouldReturnDiscountedPrice_WhenDiscountCardPresent()
         {
             // Arrange
-            TicketPricing.PricingTable = PricingTable;
             MethodInfo? methodInfo = typeof(TicketPricing).GetMethod(
                 "CalculatePriceFromDiscount",
                 BindingFlags.NonPublic | BindingFlags.Static
             );
             bool childUnder16Present = false;
-            object[] parameters = { FromCity, ToCity, DiscountCard, childUnder16Present };
+            double basePrice = 50.0;
             double discountedPrice = 45.0;
             double tolerance = 0.0001;
-
+            object[] parameters = { basePrice, DiscountCard, childUnder16Present };
+            
             // Act
             object? result = methodInfo?.Invoke(null, parameters);
             double? resultPrice = (double?)result;
@@ -224,15 +143,14 @@ namespace VVPS.UnitTests
         public void CalculatePriceFromDiscount_ShouldReturnBasePrice_WhenDiscountCardIsNull()
         {
             // Arrange
-            TicketPricing.PricingTable = PricingTable;
             MethodInfo? methodInfo = typeof(TicketPricing).GetMethod(
                 "CalculatePriceFromDiscount",
                 BindingFlags.NonPublic | BindingFlags.Static
             );
             bool childUnder16Present = false;
-            object?[] parameters = { FromCity, ToCity, null, childUnder16Present };
             double tolerance = 0.0001;
             double basePrice = 50.0;
+            object?[] parameters = { basePrice, null, childUnder16Present };
 
             // Act
             object? result = methodInfo?.Invoke(null, parameters);
@@ -250,13 +168,13 @@ namespace VVPS.UnitTests
         public void CalculateTotalPrice_ShouldReturnLowerDiscountedPrice_WhenDiscountCardIsNull()
         {
             // Arrange
-            TicketPricing.PricingTable = PricingTable;
             Ticket ticket = new(null, FromCity, ToCity, false, false);
+            double basePrice = 50.0;
             double discountedPrice = 47.5;
             double tolerance = 0.0001;
 
             // Act
-            double resultPrice = TicketPricing.CalculateTotalPrice(ticket);
+            double resultPrice = TicketPricing.CalculateTotalPrice(ticket, basePrice);
 
             // Assert
             Assert.That(resultPrice, Is.GreaterThanOrEqualTo(discountedPrice).Within(tolerance));
@@ -266,14 +184,15 @@ namespace VVPS.UnitTests
         public void CalculateTotalPrice_ShouldReturnHigherDiscountedPrice_WhenDiscountCardPresent()
         {
             // Arrange
-            TicketPricing.PricingTable = PricingTable;
             Ticket ticket = new(null, FromCity, ToCity, true, false);
             ticket.UsedDiscountCard = DiscountCard;
+
+            double basePrice = 50.0;
             double discountedPrice = 90.0;
             double tolerance = 0.0001;
 
             // Act
-            double resultPrice = TicketPricing.CalculateTotalPrice(ticket);
+            double resultPrice = TicketPricing.CalculateTotalPrice(ticket, basePrice);
 
             // Assert
             Assert.That(resultPrice, Is.LessThanOrEqualTo(discountedPrice).Within(tolerance));
